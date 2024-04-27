96d1f63a4b2f6da09d67041741d518777694c53e
Pedro <ph_tavares@hotmail.com>
2024-04-27 10:11:33 -0300

    KLAB-0001

--- frontend/src/app/inicio/inicio.module.ts
+++ frontend/src/app/inicio/inicio.module.ts
@@ -3,11 +3,12 @@ import {CommonModule} from "@angular/common";
 import {InicioComponent} from "./inicio.component";
 import {RouterModule} from "@angular/router";
 import {HttpserviceModule} from "../modules/httpservice.module";
+import { ProdutosModule } from "../produtos/produtos.module";
 
 const route = [{path: '', component: InicioComponent}]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(route), HttpserviceModule],
+  imports: [CommonModule, RouterModule.forChild(route), HttpserviceModule, ProdutosModule],
   declarations: [InicioComponent]
 })
 export class InicioModule {}
